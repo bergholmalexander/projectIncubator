033b237b239bff500cda61d9d82aa0ef59fc34c7
Mingyu Gao <mingyu.gao96@gmail.com>
2019-10-20 17:04:57 -0700

    add react

---
+++ client/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Home = () => (
+  <div>
+  </div>
+);
+
+const mapStateToProps = state => {
+  return {};
+};
+
+const mapDispatchToProps = dispatch => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
